7d4c56f5b966844425577e8dd61484192f441292
smolbrain <randomlyrandamu@gmail.com>
2023-10-04 23:18:51 +1100

    supabase auth setup

---
+++ backend/src/index.ts
@@ -0,0 +1,46 @@
+import Fastify from 'fastify'
+import { SupabaseClient } from '@supabase/supabase-js'
+import SupabaseService from './utils/Supabase.service'
+
+const fastify = Fastify({
+  logger: true,
+})
+
+const supabase = (): SupabaseClient => {
+  return SupabaseService.getInstance()
+}
+
+fastify.get('/collectible', async (req, reply) => {
+  try {
+    const token = req.headers['authorization']
+    const {
+      data: { user },
+    } = await supabase().auth.getUser(token)
+
+    const { data, error } = await supabase()
+      .from('collectibles')
+      .select()
+      .eq('user_id', user?.id)
+      .limit(1)
+
+    if (error) {
+      throw new Error('Failed to fetch collectible')
+    }
+
+    reply.send(data)
+  } catch (error) {
+    reply.status(500).send({ error: error })
+  }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+    console.log('Server started')
+  } catch (error) {
+    console.error('Error starting server:', error)
+    process.exit(1)
+  }
+}
+
+start()
