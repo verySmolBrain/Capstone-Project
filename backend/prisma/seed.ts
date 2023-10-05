fc11dfa52766bafd32a0e9b91db7aea7266522f5
smolbrain <randomlyrandamu@gmail.com>
2023-10-05 20:11:52 +1100

    prisma migration

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,15 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
-  const brain = await prisma.user.upsert({
-    where: { email: 'randomlyrandamu@gmail.com' },
-    update: {},
-    create: {
+  const brain = await prisma.user.create({
+    data: {
       id: '4be5aa5e-4245-46e6-84ed-ed2c410e7c83',
       name: 'brain',
       profile: {
         create: {
-          id: '4be5aa5e-4245-46e6-84ed-ed2c410e7c83',
           description: 'im such a goomba',
           image: 'thisisaurl',
           achievements: {},
@@ -18,17 +15,14 @@ async function main() {
           reputation: 0
         },
       },
-    },
+    }
   })
-  const goomba = await prisma.user.upsert({
-    where: { email: 'randomlyrandamu@gmail.com' },
-    update: {},
-    create: {
-      id: '4be5aa5e-4245-46e6-84ed-ed2c410e7c83',
+  const goomba = await prisma.user.create({
+    data: {
+      id: '4be5aa5e-4245-46e6-84ed-ed2c410e7c82',
       name: 'goomba',
       profile: {
         create: {
-          id: '4be5aa5e-4245-46e6-84ed-ed2c410e7c83',
           description: 'im such a brain',
           image: 'thisisaurlaswell',
           achievements: {},
@@ -37,7 +31,7 @@ async function main() {
           reputation: 69
         },
       },
-    },
+    }
   })
   console.log({ brain, goomba })
 }
