62efbd84d1b5c62ff32ee32301186b3439ee0653
William Luu <z5359552@ad.unsw.edu.au>
2023-10-07 01:15:11 +1100

    added some users + collectables to the database

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,14 +1,41 @@
 import { PrismaClient } from '@prisma/client'
+
+// Brain owns 4 pieces of Exodia
+// Jin owns the final piece of Exodia
+// Goomba owns Blue-Eyes White Dragon and Bellsprout
+
 const prisma = new PrismaClient()
+
 async function main() {
+
   const brain = await prisma.user.create({
     data: {
       id: '4be5aa5e-4245-46e6-84ed-ed2c410e7c83',
-      name: 'brain',
       profile: {
         create: {
+          name: 'brain',
           description: 'im such a goomba',
           image: 'thisisaurl',
+          collection: {
+            create: [
+              {
+                id: '1',
+                name: 'Right Arm Of The Forbidden One',
+              },
+              {
+                id: '2',
+                name: 'Left Arm Of The Forbidden One',
+              },
+              {
+                id: '3',
+                name: 'Right Leg Of The Forbidden One',
+              },
+              {
+                id: '4',
+                name: 'Left Leg Of The Forbidden One',
+              }
+            ]
+          },
           achievements: {},
           sales: {},
           purchases: {},
@@ -19,12 +46,24 @@ async function main() {
   })
   const goomba = await prisma.user.create({
     data: {
-      id: '4be5aa5e-4245-46e6-84ed-ed2c410e7c82',
-      name: 'goomba',
+      id: '50f678ab-e4a2-4066-83ee-f87d2156adbf',
       profile: {
         create: {
+          name: 'goomba',
           description: 'im such a brain',
           image: 'thisisaurlaswell',
+          collection: {
+            create: [
+              {
+                id: '6',
+                name: 'Blue-Eyes White Dragon',
+              },
+              {
+                id: '69',
+                name: 'Bellsprout',
+              },
+            ]
+          },
           achievements: {},
           sales: {},
           purchases: {},
@@ -33,7 +72,32 @@ async function main() {
       },
     },
   })
-  console.log({ brain, goomba })
+  const jin = await prisma.user.create({
+    data: {
+      id: '6e4fbc17-c6f5-431b-8a97-565d671cfb88',
+      profile: {
+        create: {
+          name: 'jin',
+          description: 'inktober',
+          image: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+          collection: {
+            create: [
+              {
+                id: '5',
+                name: 'Exodia The Forbidden One',
+              },
+            ]
+          },
+          achievements: {},
+          sales: {},
+          purchases: {},
+          reputation: 5,
+        },
+      },
+    },
+  })
+
+  console.log(brain, goomba, jin)
 }
 main()
   .then(async () => {
