f6a827c63b65b8fdfb2db5949fbfb9333e5b68e0
Brendon Zhao <brendon.zhao@gmail.com>
2023-10-08 16:13:34 +1100

    added endpoint for sending message

--- backend/src/chat.ts
+++ backend/src/chat.ts
@@ -33,7 +33,7 @@ async function getChat(token: string | undefined, userId: string | undefined, re
 
 async function updateChat(token: string | undefined, userId: string | undefined, receiverId: string | undefined) {
   const prisma = await requestHandler(token as string)
-  const post = await prisma.chat.create({
+  const newChat = await prisma.chat.create({
     data: {
       users: {
         connect: [
@@ -47,7 +47,35 @@ async function updateChat(token: string | undefined, userId: string | undefined,
       },
     },
   })
-  return post
+  return newChat
 }
 
-export {getChats, getChat, updateChat}
+async function sendMessage(token: string | undefined, userId: string | undefined, receiverId: string | undefined, message: string) {
+  const prisma = await requestHandler(token as string)
+
+  const currChat = await prisma.chat.update({
+    where: {
+      users: {
+        every: {
+          id: {
+            in: [userId, receiverId]
+          }
+        }
+      }
+    }
+  })
+
+  const newMessage = await prisma.message.create({
+    data: {
+      chatId: currChat.id,
+      senderId: userId,
+      receiverId: receiverId,
+      content: message
+    }
+  })
+  currChat.messages.push(newMessage)
+
+  return currChat
+}
+
+export {getChats, getChat, updateChat, sendMessage}
