15e629cf569d70fcccdd1a10ca767333fc6f2f94
smolbrain <randomlyrandamu@gmail.com>
2023-10-19 03:07:24 +1100

    polish

--- backend/tests/__mocks__/utils/PrismaHandler.ts
+++ backend/tests/__mocks__/utils/PrismaHandler.ts
@@ -1,26 +1,25 @@
 import { PrismaClient } from '@prisma/client'
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
 
-import { requestHandler, getUserId } from '@Source/utils/SupabaseUtils'
+import { requestHandler, supabase } from '@Source/utils/supabaseUtils'
+import { getUserId } from '@Source/utils/utils'
 import SupabaseService from '@Source/utils/Supabase.service'
 
 const prismaMock = mockDeep<PrismaClient>()
 
-jest.mock('@Source/utils/PrismaHandler', () => ({
+jest.mock('@Source/utils/supabaseUtils', () => ({
   requestHandler: jest.fn(() => Promise.resolve(prismaMock)),
   supabase: jest.fn(() => SupabaseService.getInstance()),
-  getUserId: jest.fn(() => Promise.resolve('')),
+  validateUser: jest.fn(() => Promise.resolve(true)),
+  extractId: jest.fn(() => ''),
+  // getUserId: jest.fn(() => Promise.resolve('')),
 }))
 
 afterEach(() => {
   mockReset(requestHandler)
   mockReset(prismaMock)
-  mockReset(getUserId)
+  mockReset(supabase)
 })
 
-export const prismaMockInstance = prismaMock as unknown as DeepMockProxy<
-  typeof prismaMock
->
-export const getUserIdMock = getUserId as unknown as jest.MockedFunction<
-  typeof getUserId
->
+export const prismaMockInstance = prismaMock as unknown as DeepMockProxy<typeof prismaMock>
+export const getUserIdMock = getUserId as unknown as jest.MockedFunction<typeof getUserId>
--- backend/tests/routes/chat.test.ts
+++ backend/tests/routes/chat.test.ts
@@ -4,25 +4,26 @@ import { prismaMockInstance } from '@Test/__mocks__/utils/PrismaHandler'
 describe('Chat', () => {
   it('should return 200', async () => {
     prismaMockInstance.chat.findMany.mockResolvedValueOnce([
-      // @ts-expect-error findMany generates type based on query so it will error
-      { id: 1, users: [{ sender: 1 }] },
-    ])
-    prismaMockInstance.message.findMany.mockResolvedValueOnce([
       {
         id: 1,
-        chatId: 1,
-        senderId: 'sender',
-        receiverId: 'receiver',
-        content: 'Hello',
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        // @ts-expect-error findMany generates type based on query so it will error
+        users: [{ sender: 1 }],
+        messages: {
+          id: 1,
+          chatId: 1,
+          senderId: 'sender',
+          content: 'Hello',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          messages: [],
+        },
       },
     ])
 
     const app = await build({})
     const response = await app.inject({
       method: 'GET',
-      url: '/chats',
+      url: '/chat',
       headers: {
         Authorization: 'Bearer your-token-here',
       },
