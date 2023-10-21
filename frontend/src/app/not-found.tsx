e1a4e1be6f22a89abb0009edd1056e3f1e478103
Brendon Zhao <brendon.zhao@gmail.com>
2023-10-21 21:54:06 +1100

    added logo

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,9 +1,11 @@
+import { IconLogo } from '@/components/ui/assets/IconLogo'
 import Link from 'next/link'
 
 export default function Custom404() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <h2 className="text-3xl font-semibold tracking-tight">Uh oh 😕</h2>
+      <IconLogo className="w-16 h-16 mx-auto" />
+      <h2 className="text-3xl font-semibold tracking-tight">Uh oh!</h2>
       <p className="text-muted-foreground">
         We couldn&apos;t find what you were after.
       </p>
