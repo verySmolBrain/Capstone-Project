d2cdbf9bd35f4e84928a88077ec62ee55e87ca15
smolbrain <randomlyrandamu@gmail.com>
2023-10-28 00:37:16 +1100

    updated profile page

--- frontend/src/components/ui/button/add-friend-button.tsx
+++ frontend/src/components/ui/button/add-friend-button.tsx
@@ -2,15 +2,15 @@
 
 import * as React from 'react'
 import Link from 'next/link'
-import { Pencil } from 'lucide-react'
+import { UserPlus } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 
-export function ProfileEditButton() {
+export function AddFriendButton() {
   return (
     <Link href="/settings">
       <Button variant="outline" size="icon">
-        <Pencil className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+        <UserPlus className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
       </Button>
     </Link>
   )
