3311fb91064493cc7cb6656d6edf5bd109a11ac1
William Luu <z5359552@ad.unsw.edu.au>
2023-10-30 03:25:32 +1100

    adding/removing collectables from collections

--- frontend/src/components/ui/button/remove-collectable-from-collection-button.tsx
+++ frontend/src/components/ui/button/remove-collectable-from-collection-button.tsx
@@ -10,22 +10,80 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Database } from '@/lib/database.types'
+import { toast } from '../use-toast'
 
-export function RemoveCollectableFromCollectionButton() {
+async function onDelete(
+  collection: string,
+  collectable: string,
+  mutate: () => void
+) {
+  const supabase = createClientComponentClient<Database>()
+  const token = (await supabase.auth.getSession()).data.session?.access_token
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_HOSTNAME}/collection/${collection}/${collectable}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: token!,
+      },
+    }
+  )
+
+  if (!response?.ok) {
+    return toast({
+      title: 'Uh Oh! Something went wrong!',
+      description: response?.statusText,
+      variant: 'destructive',
+    })
+  }
+  mutate()
+  return toast({
+    title: 'Success!',
+    description:
+      'The collectable was successfully removed from the collection!',
+    variant: 'default',
+  })
+}
+
+export function RemoveCollectableCollectionButton(props: {
+  collection: string
+  collectable: string
+  mutate: () => void
+}) {
   return (
     <Dialog>
       <DialogTrigger asChild className="hidden group-hover:block">
-        <Button className="absolute right-4 top-4">
-          <X className="h-4 w-4" />
+        <Button className="absolute right-0 top-0 p-2">
+          <X className="h-6 w-6" />
         </Button>
       </DialogTrigger>
-      <DialogContent className="w-auto">
+      <DialogContent className="w-auto m-20">
         <DialogHeader>
           <DialogTitle>
-            Are you sure you want to move this to the PC?
+            Are you sure you want to move this item to the PC?
           </DialogTitle>
         </DialogHeader>
-        <div>yes/no</div>
+        <div className="flex-col justify-left">
+          <DialogTrigger asChild className="group-hover:block">
+            <Button
+              className="flex-col m-2"
+              onClick={() => {
+                onDelete(props.collection, props.collectable, props.mutate)
+              }}
+            >
+              Yes
+            </Button>
+          </DialogTrigger>
+          <DialogTrigger asChild className="group-hover:block">
+            <Button variant="secondary" className="flex-col m-2">
+              Cancel
+            </Button>
+          </DialogTrigger>
+        </div>
       </DialogContent>
     </Dialog>
   )
