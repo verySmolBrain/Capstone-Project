6623faef1e07478d6c452920720581f4b08e7d99
Brendon Zhao <brendon.zhao@gmail.com>
2023-11-15 16:02:47 +1100

    removed redirect buttons and replaced with redirect button framework

---
+++ frontend/src/components/ui/button/redirect-button.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import Link from 'next/link'
+import { LucideIcon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+
+// General button framework for redirect buttons: takes in a Lucide Icon and the url to redirect to
+export function RedirectButton({
+  url,
+  Icon,
+  text,
+}: {
+  url: string
+  Icon: LucideIcon
+  text?:
+    | {
+        variant:
+          | 'default'
+          | 'link'
+          | 'destructive'
+          | 'outline'
+          | 'secondary'
+          | 'ghost'
+          | null
+          | undefined
+        content: string
+        size: 'sm' | 'lg' | 'default' | 'icon' | null | undefined
+        params: string
+      }
+    | undefined
+}) {
+  return (
+    <Link href={url}>
+      {text ? (
+        <Button variant={text.variant} size={text.size} className={text.params}>
+          {text.content}
+        </Button>
+      ) : (
+        <Button variant="outline" size="icon">
+          <Icon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+        </Button>
+      )}
+    </Link>
+  )
+}
